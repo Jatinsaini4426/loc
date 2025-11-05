1eb2a18c6e9a48cb0f5ab8bd0494e4d7b5e9115a
jatinsaini <jatinsaini@cssoftsolutions.com>
2025-11-05 15:43:38 +0530

    remove cors

--- api/GLT.js
+++ api/GLT.js
@@ -7,10 +7,6 @@ router.use(express.json());
 
 // Allow CORS for all routes
 router.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  if (req.method === "OPTIONS") return res.status(200).end();
   next();
 });
 
